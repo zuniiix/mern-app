0d922b2d187ef99aa37c4b3c4cd52a7b7565a226
zuniiix <lisamzuni143@gmail.com>
2024-01-23 15:46:02 -0600

    String is shown in browser change

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
 
 // express app stored in that constant
 const app = express()
 
 // we want to react to requests so set up a route handler , aka the route being app
-app.get()
+app.get('/', (req, res) => { 
+    res.json({mssg: 'Welcome to the app'})
+})
 
-// type npm run dev to use nodemon,, we can see this in package.json line 9
 // listen for port numbers ,, requests
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
 })
